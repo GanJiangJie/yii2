825edb712bfe6ad5b58b95510bec3663be1fcfe1
ganjiangjie <ganjiangjie@sina.com>
2021-09-14 21:07:14 +0800

    初始化

--- common/util/Single/Request.php
+++ common/util/Single/Request.php
@@ -38,6 +38,35 @@ class Request
     }
 
     /**
+     * 获取客户端IP地址
+     * @return null|string
+     */
+    public function ip()
+    {
+        // check for shared internet/ISP IP
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            // check for IPs passing through proxies
+            // check if multiple ips exist in var
+            @list($ip) = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            return $ip;
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
+            return $_SERVER['HTTP_X_FORWARDED'];
+        } elseif (isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+            return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        } elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_FORWARDED_FOR'];
+        } elseif (isset($_SERVER['HTTP_FORWARDED'])) {
+            return $_SERVER['HTTP_FORWARDED'];
+        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            return $_SERVER['REMOTE_ADDR'];
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
      * 获取请求头的信息
      * @param string $key
      * @param string|int $default
