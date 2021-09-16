6c6d9dac4ad5fe02a627427f398b2cee31d7f7dd
ganjiangjie <ganjiangjie@sina.com>
2021-09-16 21:51:45 +0800

    初始化

--- common/helpers.php
+++ common/helpers.php
@@ -22,9 +22,11 @@ if (!function_exists('tbe')) {
      */
     function tbe(string $message, int $code = API_ERROR_CODE_FAIL)
     {
+        $msg = $message ?: ($GLOBALS['__API_ERROR_CODE'][$code] ?? $GLOBALS['__API_ERROR_CODE'][API_ERROR_CODE_FAIL]);
         @list($back, $trace) = debug_backtrace();
         exception()->backtrace($back ?? [], $trace ?? []);
-        throw new \yii\base\Exception($message, $code);
+        exception()->message = $msg;
+        throw new \yii\base\Exception($msg, $code);
     }
 }
 
