e945529c74e946c1e89169bd50ee355ba4c186bf
ganjiangjie <ganjiangjie@sina.com>
2021-11-25 22:04:33 +0800

    初始化

--- routes/member.php
+++ routes/member.php
@@ -1,10 +1,12 @@
 <?php
 
 route()->before([
-    //'auth',
-    'mThrottle' . config('params.throttle.api.common')
+    'auth',
 ], [
     'crm.member.list' => 'crm/member/list',
+])->before([
+    'mThrottle' . config('params.throttle.api.common')
+], [
     'crm.member.edit' => 'crm/member/edit',
 ])->before([
     'uThrottle' . config('params.throttle.member_register'),
